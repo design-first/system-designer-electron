7d00ca0a19bb21d72ea6689382b2030953c0ce60
Erwan Carriou <erwan.carriou@mac.com>
2017-11-19 12:38:51 +0100

    clean code

--- main.js
+++ main.js
@@ -27,277 +27,277 @@ let mainWindow;
 
 function createWindow() {
 
-    var Menu = electron.Menu;
+  var Menu = electron.Menu;
 
-    var template = [
+  var template = [
+    {
+      label: 'Edit',
+      submenu: [
         {
-            label: 'Edit',
-            submenu: [
-                {
-                    label: 'Undo',
-                    accelerator: 'CmdOrCtrl+Z',
-                    role: 'undo'
-                },
-                {
-                    label: 'Redo',
-                    accelerator: 'Shift+CmdOrCtrl+Z',
-                    role: 'redo'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Cut',
-                    accelerator: 'CmdOrCtrl+X',
-                    role: 'cut'
-                },
-                {
-                    label: 'Copy',
-                    accelerator: 'CmdOrCtrl+C',
-                    role: 'copy'
-                },
-                {
-                    label: 'Paste',
-                    accelerator: 'CmdOrCtrl+V',
-                    role: 'paste'
-                },
-                {
-                    label: 'Select All',
-                    accelerator: 'CmdOrCtrl+A',
-                    role: 'selectall'
-                }
-            ]
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
         },
         {
-            label: 'View',
-            submenu: [
-                {
-                    label: 'Reload',
-                    accelerator: 'CmdOrCtrl+R',
-                    click: function (item, focusedWindow) {
-                        if (focusedWindow)
-                            focusedWindow.reload();
-                    }
-                },
-                {
-                    label: 'Toggle Full Screen',
-                    accelerator: (function () {
-                        if (process.platform == 'darwin')
-                            return 'Ctrl+Command+F';
-                        else
-                            return 'F11';
-                    })(),
-                    click: function (item, focusedWindow) {
-                        if (focusedWindow)
-                            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-                    }
-                },
-                {
-                    label: 'Toggle Developer Tools',
-                    accelerator: (function () {
-                        if (process.platform == 'darwin')
-                            return 'Alt+Command+I';
-                        else
-                            return 'Ctrl+Shift+I';
-                    })(),
-                    click: function (item, focusedWindow) {
-                        if (focusedWindow)
-                            focusedWindow.toggleDevTools();
-                    }
-                }
-            ]
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
         },
         {
-            label: 'Window',
-            role: 'window',
-            submenu: [
-                {
-                    label: 'Minimize',
-                    accelerator: 'CmdOrCtrl+M',
-                    role: 'minimize'
-                },
-                {
-                    label: 'Close',
-                    accelerator: 'CmdOrCtrl+W',
-                    role: 'close'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Next Window',
-                    accelerator: 'Ctrl+Tab',
-                    click: function (item, focusedWindow) {
-                        nextWindow();
-                    }
-                },
-                {
-                    label: 'Previous Window',
-                    accelerator: 'Ctrl+Shift+Tab',
-                    click: function (item, focusedWindow) {
-                        previousWindow();
-                    }
-                },
-                {
-                    type: 'separator'
-                }, {
-                    label: 'System Designer',
-                    click: function (item, focusedWindow) {
-                        if (!mainWindow) {
-                            createWindow();
-                        } else {
-                            mainWindow.focus();
-                        }
-                    }
-                }
-            ]
+          type: 'separator'
         },
         {
-            label: 'Help',
-            role: 'help',
-            submenu: [
-                {
-                    label: 'Read the documentation',
-                    click: function () { require('electron').shell.openExternal('https://system-designer.readme.io') }
-                },
-                {
-                    label: 'Ask a question',
-                    click: function () { require('electron').shell.openExternal('https://system-designer.readme.io/discuss') }
-                },
-                {
-                    label: 'Report a bug',
-                    click: function () { require('electron').shell.openExternal('https://github.com/design-first/system-designer/issues') }
-                },
-                {
-                    label: 'Go to the website',
-                    click: function () { require('electron').shell.openExternal('https://designfirst.io/systemdesigner/') }
-                }
-            ]
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
         }
-    ];
-
-    if (process.platform == 'darwin') {
-        var name = 'System Designer';
-        template.unshift({
-            label: name,
-            submenu: [
-                {
-                    label: 'About ' + name,
-                    role: 'about'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Services',
-                    role: 'services',
-                    submenu: []
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Hide ' + name,
-                    accelerator: 'Command+H',
-                    role: 'hide'
-                },
-                {
-                    label: 'Hide Others',
-                    accelerator: 'Command+Alt+H',
-                    role: 'hideothers'
-                },
-                {
-                    label: 'Show All',
-                    role: 'unhide'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Quit',
-                    accelerator: 'Command+Q',
-                    click: function () { app.quit(); }
-                },
-            ]
-        });
-        // Window menu.
-        template[3].submenu.push(
-            {
-                type: 'separator'
-            },
-            {
-                label: 'Bring All to Front',
-                role: 'front'
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function (item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function () {
+            if (process.platform == 'darwin')
+              return 'Ctrl+Command+F';
+            else
+              return 'F11';
+          })(),
+          click: function (item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: (function () {
+            if (process.platform == 'darwin')
+              return 'Alt+Command+I';
+            else
+              return 'Ctrl+Shift+I';
+          })(),
+          click: function (item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.toggleDevTools();
+          }
+        }
+      ]
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Next Window',
+          accelerator: 'Ctrl+Tab',
+          click: function (item, focusedWindow) {
+            nextWindow();
+          }
+        },
+        {
+          label: 'Previous Window',
+          accelerator: 'Ctrl+Shift+Tab',
+          click: function (item, focusedWindow) {
+            previousWindow();
+          }
+        },
+        {
+          type: 'separator'
+        }, {
+          label: 'System Designer',
+          click: function (item, focusedWindow) {
+            if (!mainWindow) {
+              createWindow();
+            } else {
+              mainWindow.focus();
             }
-        );
+          }
+        }
+      ]
+    },
+    {
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Read the documentation',
+          click: function () { require('electron').shell.openExternal('https://system-designer.readme.io') }
+        },
+        {
+          label: 'Ask a question',
+          click: function () { require('electron').shell.openExternal('https://system-designer.readme.io/discuss') }
+        },
+        {
+          label: 'Report a bug',
+          click: function () { require('electron').shell.openExternal('https://github.com/design-first/system-designer/issues') }
+        },
+        {
+          label: 'Go to the website',
+          click: function () { require('electron').shell.openExternal('https://designfirst.io/systemdesigner/') }
+        }
+      ]
     }
+  ];
 
-    var menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-
-    mainWindow = new BrowserWindow({
-        width: 1024,
-        height: 768
+  if (process.platform == 'darwin') {
+    var name = 'System Designer';
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          label: 'About ' + name,
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Services',
+          role: 'services',
+          submenu: []
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Hide ' + name,
+          accelerator: 'Command+H',
+          role: 'hide'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Alt+H',
+          role: 'hideothers'
+        },
+        {
+          label: 'Show All',
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function () { app.quit(); }
+        },
+      ]
     });
-    mainWindow.loadURL('file://' + __dirname + '/designer/index.html');
+    // Window menu.
+    template[3].submenu.push(
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Bring All to Front',
+        role: 'front'
+      }
+    );
+  }
 
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
+  var menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768
+  });
+  mainWindow.loadURL('file://' + __dirname + '/designer/index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow();
-    }
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 function nextWindow() {
-    var windows = BrowserWindow.getAllWindows(),
-        focused = BrowserWindow.getFocusedWindow(),
-        index = 0,
-        lastIndex = 0,
-        nextIndex = 0;
+  var windows = BrowserWindow.getAllWindows(),
+    focused = BrowserWindow.getFocusedWindow(),
+    index = 0,
+    lastIndex = 0,
+    nextIndex = 0;
 
-    if (focused) {
-        index = windows.map(win => win.id).indexOf(focused.id);
-        lastIndex = windows.length - 1;
+  if (focused) {
+    index = windows.map(win => win.id).indexOf(focused.id);
+    lastIndex = windows.length - 1;
 
-        if (index + 1 > lastIndex) {
-            nextIndex = 0;
-        } else {
-            nextIndex = index + 1;
-        }
-
-        windows[nextIndex].focus();
+    if (index + 1 > lastIndex) {
+      nextIndex = 0;
+    } else {
+      nextIndex = index + 1;
     }
+
+    windows[nextIndex].focus();
+  }
 }
 
 function previousWindow() {
-    var windows = BrowserWindow.getAllWindows(),
-        focused = BrowserWindow.getFocusedWindow(),
-        index = 0,
-        lastIndex = 0,
-        prevIndex = 0;
+  var windows = BrowserWindow.getAllWindows(),
+    focused = BrowserWindow.getFocusedWindow(),
+    index = 0,
+    lastIndex = 0,
+    prevIndex = 0;
 
-    if (focused) {
-        index = windows.map(win => win.id).indexOf(focused.id);
-        lastIndex = windows.length - 1;
+  if (focused) {
+    index = windows.map(win => win.id).indexOf(focused.id);
+    lastIndex = windows.length - 1;
 
-        if (index === 0) {
-            prevIndex = lastIndex;
-        } else {
-            prevIndex = index - 1;
-        }
-
-        windows[prevIndex].focus();
+    if (index === 0) {
+      prevIndex = lastIndex;
+    } else {
+      prevIndex = index - 1;
     }
+
+    windows[prevIndex].focus();
+  }
 }
